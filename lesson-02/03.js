aa29decdad0e4ee9d5e13c2a9c5831f210469537
Sherbina#6337 <58983300+kosmos1you@users.noreply.github.com>
2024-10-03 22:48:30 +0300

    Create 03.js

---
+++ lesson-02/03.js
@@ -0,0 +1,9 @@
+// Code
+const giveMeRightNumbers = (value1, value2, maximum) => {
+    if (value1 === value2 || value1 <= maximum) {
+        console.log('Success!');
+    } else {
+        console.log('Something wrong!');
+    }
+}
+giveMeRightNumbers(100, 150, 90)
